145d27d42bf06fb289af2c1fe9f456e511aa6b60
Cortalo <Cortalo@outlook.com>
2025-08-27 20:49:17 +0200

    Spring MVC: get request data and return response by manually write text/html

--- src/main/java/com/nowcoder/community/controller/AlphaController.java
+++ src/main/java/com/nowcoder/community/controller/AlphaController.java
@@ -6,6 +6,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
 @Controller
 @RequestMapping("/alpha")
 public class AlphaController {
@@ -23,6 +29,28 @@ public class AlphaController {
     @ResponseBody
     public String getData() {
         return alphaService.find();
+    }
+
+    @RequestMapping("/http")
+    public void http(HttpServletRequest request, HttpServletResponse response) {
+        // get request data
+        System.out.println(request.getMethod());
+        System.out.println(request.getServletPath());
+        Enumeration<String> enumeration = request.getHeaderNames();
+        while(enumeration.hasMoreElements()) {
+            String name = enumeration.nextElement();
+            String value = request.getHeader(name);
+            System.out.println(name + ": " + value);
+        }
+        System.out.println(request.getParameter("code"));
+
+        // return response data
+        response.setContentType("text/html;charset=utf-8");
+        try(PrintWriter writer = response.getWriter()) {
+            writer.write("<h1>Now Coder</h1>");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
