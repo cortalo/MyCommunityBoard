67469d7216b877c914d2fc032f3a3cc4e91ff6fb
Cortalo <Cortalo@outlook.com>
2025-08-27 19:57:39 +0200

    Spring Container: using `@Qualifier` to select bean by name.

--- src/test/java/com/nowcoder/community/CommunityApplicationTests.java
+++ src/test/java/com/nowcoder/community/CommunityApplicationTests.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
@@ -55,10 +56,19 @@ public class CommunityApplicationTests implements ApplicationContextAware {
     }
 
     @Autowired
+    @Qualifier("alphaHibernate")
     private AlphaDao alphaDao;
+
+    @Autowired
+    private AlphaService alphaService;
+
+    @Autowired
+    private SimpleDateFormat simpleDateFormat;
 
     @Test
     public void testDI() {
         System.out.println(alphaDao);
+        System.out.println(alphaService);
+        System.out.println(simpleDateFormat);
     }
 }
