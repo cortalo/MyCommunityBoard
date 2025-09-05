97f9108128bcbf6feadb17443a1b5a871717c101
Cortalo <Cortalo@outlook.com>
2025-09-05 21:36:07 +0200

    Interceptor: using interceptor to globally inject model into many controllers.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # My Community Forum
 
-TODO 2.23 login and logout
+TODO 2.27 show user information after login
---
+++ src/main/java/com/nowcoder/community/controller/interceptor/AlphaInterceptor.java
@@ -0,0 +1,37 @@
+package com.nowcoder.community.controller.interceptor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class AlphaInterceptor implements HandlerInterceptor {
+
+    private static final Logger logger = LoggerFactory.getLogger(AlphaInterceptor.class);
+
+    // it will run before controller
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        logger.debug("preHandle: " + handler.toString());
+        return true;
+    }
+
+    // it will run after controller
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        logger.debug("postHandle: " + handler.toString());
+    }
+
+    // it will run even after template
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        logger.debug("afterCompletion: " + handler.toString());
+        //HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+    }
+}
