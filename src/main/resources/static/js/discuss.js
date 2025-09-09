d18507bd450731ea2ff5233216ec2b9ad265723f
Cortalo <Cortalo@outlook.com>
2025-09-09 09:54:14 +0200

    update discuss.js to redirect to login page if it receives html response

--- src/main/resources/static/js/discuss.js
+++ src/main/resources/static/js/discuss.js
@@ -3,6 +3,13 @@ function like(btn, entityType, entityId) {
         CONTEXT_PATH + "/like",
         {"entityType":entityType,"entityId":entityId},
         function(data) {
+            // Check if response is HTML, meaning the user has not login
+            // and should redirect to login page
+            if (typeof data === 'string' && data.trim().startsWith('<!')) {
+                location.href = CONTEXT_PATH + "/login";
+                return;
+            }
+            
             data = $.parseJSON(data);
             if (data.code == 0) {
                 $(btn).children("i").text(data.likeCount);
