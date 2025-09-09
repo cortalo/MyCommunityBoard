07b407b8369720b149c9996b85d355f2599943b4
Cortalo <Cortalo@outlook.com>
2025-09-09 14:02:57 +0200

    implement profile page to show follower and followee, and the function to follow someone

--- src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
+++ src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
@@ -5,6 +5,8 @@ public class RedisKeyUtil {
     private static final String SPLIT = ":";
     private static final String PREFIX_ENTITY_LIKE = "like:entity";
     private static final String PREFIX_USER_LIKE = "like:user";
+    private static final String PREFIX_FOLLOWEE = "followee";
+    private static final String PREFIX_FOLLOWER = "follower";
 
     // some like entity
     // like:entity:entityType:entityId -> set(userId)
@@ -17,5 +19,18 @@ public class RedisKeyUtil {
     public static String getUserLikeKey(int userId) {
         return PREFIX_USER_LIKE + SPLIT + userId;
     }
+
+    // some user follow entity
+    // followee:userId:entityType -> zset(entityId,now)
+    public static String getFolloweeKey(int userId, int entityType) {
+        return PREFIX_FOLLOWEE + SPLIT + userId + SPLIT + entityType;
+    }
+
+    // some user's follower
+    // follower:entityType:entityId -> zset(userId,now)
+    public static String getFollowerKey(int entityType, int entityId) {
+        return PREFIX_FOLLOWER +SPLIT + entityType + SPLIT + entityId;
+    }
+
 
 }
