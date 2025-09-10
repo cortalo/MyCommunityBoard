82c3af8d5792c3c40693c3288f7d7b286086d8f7
Cortalo <Cortalo@outlook.com>
2025-09-10 08:25:23 +0200

    setup MyBatis for DiscussPost.

---
+++ src/test/java/com/nowcoder/community/MapperTests.java
@@ -0,0 +1,30 @@
+package com.nowcoder.community;
+
+import com.nowcoder.community.dao.DiscussPostMapper;
+import com.nowcoder.community.entity.DiscussPost;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class MapperTests {
+
+    @Autowired
+    private DiscussPostMapper discussPostMapper;
+
+    @Test
+    public void testSelectPosts() {
+        List<DiscussPost> list = discussPostMapper.selectDiscussPosts(149, 0, 10);
+        for(DiscussPost post : list) {
+            System.out.println(post);
+        }
+    }
+
+}
