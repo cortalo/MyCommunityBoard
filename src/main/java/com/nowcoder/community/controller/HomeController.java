50b8616238330040b2eab73d48599928a5a07640
Cortalo <Cortalo@outlook.com>
2025-09-11 18:36:02 +0200

    setup files for spring security demo

--- src/main/java/com/nowcoder/community/controller/HomeController.java
+++ src/main/java/com/nowcoder/community/controller/HomeController.java
@@ -9,8 +9,28 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController {
 
     @RequestMapping(path = "/index", method = RequestMethod.GET)
-    public String getIndexPage(Model model) {
+    public String getIndexPage() {
         return "/index";
+    }
+
+    @RequestMapping(path = "/discuss", method = RequestMethod.GET)
+    public String getDiscussPage() {
+        return "/site/discuss";
+    }
+
+    @RequestMapping(path = "/letter", method = RequestMethod.GET)
+    public String getLetterPage() {
+        return "/site/letter";
+    }
+
+    @RequestMapping(path = "/admin", method = RequestMethod.GET)
+    public String getAdminPage() {
+        return "/site/admin";
+    }
+
+    @RequestMapping(path = "/loginpage", method = {RequestMethod.GET, RequestMethod.POST})
+    public String getLoginPage() {
+        return "/site/login";
     }
 
 }
