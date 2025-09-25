a37a6667b83cfb9048d90f06fadeebe09cfc7ce2
Cortalo <Cortalo@outlook.com>
2025-09-25 21:13:37 +0200

    init MyBatis

---
+++ src/test/java/com/nowcoder/community/MapperTests.java
@@ -0,0 +1,27 @@
+package com.nowcoder.community;
+
+import com.nowcoder.community.dao.DiscussPostMapper;
+import com.nowcoder.community.entity.DiscussPost;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+public class MapperTests {
+
+    @Autowired
+    private DiscussPostMapper discussPostMapper;
+
+    @Test
+    public void testSelectPosts() {
+        List<DiscussPost> list = discussPostMapper.selectDiscussPosts(149, 0, 10);
+        for(DiscussPost post: list) {
+            System.out.println(post);
+        }
+        Assertions.assertEquals(1,1);
+    }
+
+}
