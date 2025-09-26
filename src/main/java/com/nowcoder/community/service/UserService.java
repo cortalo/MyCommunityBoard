2f4d435d06c1c1b17d51a4a067937b76b7a5aeac
Cortalo <Cortalo@outlook.com>
2025-09-26 09:46:53 +0200

    finish register function

--- src/main/java/com/nowcoder/community/service/UserService.java
+++ src/main/java/com/nowcoder/community/service/UserService.java
@@ -2,8 +2,19 @@ package com.nowcoder.community.service;
 
 import com.nowcoder.community.dao.UserMapper;
 import com.nowcoder.community.entity.User;
+import com.nowcoder.community.util.CommunityUtil;
+import com.nowcoder.community.util.MailClient;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
 @Service
 public class UserService {
@@ -11,12 +22,86 @@ public class UserService {
     @Autowired
     private UserMapper userMapper;
 
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Autowired
+    private MailClient mailClient;
+
+    @Value("${community.path.domain}")
+    private String domain;
+
+    @Value("${server.servlet.context-path}")
+    private String contextPath;
+
     public User findUserById(int id) {
         return userMapper.selectById(id);
     }
 
     public User findUserByUsername(String username) {
         return userMapper.selectByUsername(username);
+    }
+
+    /**
+     * try to add the new user to database, and send an activation email.
+     * Return a null map if success, otherwise return error messages in the map.
+     * @param user: user
+     * @return the map may have key as usernameMsg, passwordMsg and emailMsg.
+     */
+    public Map<String, Object> register(User user) {
+        Map<String, Object> map = new HashMap<>();
+
+        // null value handling
+        if (user == null) {
+            throw new IllegalArgumentException("user cannot be null");
+        }
+        if (StringUtils.isBlank(user.getUsername())) {
+            map.put("usernameMsg", "username cannot be empty");
+            return map;
+        }
+        if (StringUtils.isBlank(user.getPassword())) {
+            map.put("passwordMsg", "password cannot be empty");
+            return map;
+        }
+        if (StringUtils.isBlank(user.getEmail())) {
+            map.put("emailMsg", "email cannot be empty");
+            return map;
+        }
+
+        // check if username has been registered
+        User u = userMapper.selectByUsername(user.getUsername());
+        if (u != null) {
+            map.put("usernameMsg", "username exists, please use a different username");
+            return map;
+        }
+
+        // check if email has been registered
+        u = userMapper.selectByEmail(user.getEmail());
+        if (u != null) {
+            map.put("emailMsg", "email exists, please use a different email");
+            return map;
+        }
+
+        // register
+        user.setSalt(CommunityUtil.generateUUID().substring(0,5));
+        user.setPassword(CommunityUtil.md5(user.getPassword() + user.getSalt()));
+        user.setType(0);
+        user.setStatus(0);
+        user.setActivationCode(CommunityUtil.generateUUID());
+        user.setHeaderUrl(String.format("http://images.nowcoder.com/head/%dt.png", new Random().nextInt(1000)));
+        user.setCreateTime(new Date());
+        userMapper.insertUser(user);
+
+        // sending activation email
+        Context context = new Context();
+        context.setVariable("email", user.getEmail());
+        // http://localhost:8080/community/activation/101/code
+        String url = domain + contextPath + "/activation/" + user.getId() + "/" + user.getActivationCode();
+        context.setVariable("url", url);
+        String content = templateEngine.process("/mail/activation", context);
+        mailClient.sendMail(user.getEmail(), "Activate Your Account", content);
+
+        return map;
     }
 
 }
