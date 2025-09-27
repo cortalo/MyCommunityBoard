197c4c25cd109f35a53d2397a17245ad577c50d0
Cortalo <Cortalo@outlook.com>
2025-09-27 17:00:45 +0200

    implement delete message using th:data-letter-id in button

--- src/main/java/com/nowcoder/community/controller/MessageController.java
+++ src/main/java/com/nowcoder/community/controller/MessageController.java
@@ -92,6 +92,13 @@ public class MessageController {
         return "/site/letter-detail";
     }
 
+    @RequestMapping(path = "/letter/delete", method = RequestMethod.POST)
+    @ResponseBody
+    public String deletaMessage(int id) {
+        messageService.deleteMessage(id);
+        return CommunityUtil.getJSONString(0);
+    }
+
     private User getLetterTarget(String conversationId) {
         String[] ids = conversationId.split("_");
         int id0 = Integer.parseInt(ids[0]);
