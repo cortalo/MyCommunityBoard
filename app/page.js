901c11b0e157492d4623ae4fa700c872f231b7e4
Cortalo <Cortalo@outlook.com>
2025-12-08 22:47:31 +0100

    init homepage

--- app/page.js
+++ app/page.js
@@ -1,5 +1,31 @@
+import Image from "next/image";
+import Link from "next/link";
+import Pagination from "./_components/Pagination";
+import PostItem from "./_components/PostItem";
+import Filter from "./_components/Filter";
+
 function page() {
-  return <div>Hello</div>;
+  return (
+    <div className="main">
+      <div className="container">
+        <div className="position-relative">
+          <Filter />
+          <button
+            type="button"
+            className="btn btn-primary btn-sm position-absolute rt-0"
+            data-toggle="modal"
+            data-target="#publishModal"
+          >
+            publish
+          </button>
+        </div>
+        {/* pop up window for publish */}
+
+        <PostItem />
+        <Pagination />
+      </div>
+    </div>
+  );
 }
 
 export default page;
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "images.nowcoder.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
